519e875f8c87a267db544c54690fab829a736947
YUUKIToriyama <github@toriyama.dev>
2022-04-20 23:02:41 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import fastify from 'fastify';
+import fastifyStatic from 'fastify-static';
+import path from 'path';
+
+const ROOT_DIR = process.argv[2] || "./";
+const PORT = process.argv[3] || "8080";
+
+const server = fastify({
+	logger: true
+});
+
+server.register(fastifyStatic, {
+	root: path.join(process.env.PWD, ROOT_DIR),
+	index: false,
+	list: true,
+});
+
+server.listen(PORT, (err, address) => {
+	if (err) {
+		throw err
+	}
+	server.log.info(`server listening on ${address}`);
+});

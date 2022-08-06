af8268d0d4f9dd6a4fdf58b0cf67b657bd976568
YUUKIToriyama <github@toriyama.dev>
2022-08-07 01:01:13 +0900

    ファイルの一覧をHTMLで表示するようにした

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import fastify from 'fastify';
 import fastifyStatic from 'fastify-static';
 import path from 'path';
+import { Directory, File, FileListRenderer } from './utils';
 
 const ROOT_DIR = process.argv[2] || "./";
 const PORT = process.argv[3] || "8080";
@@ -10,10 +11,23 @@ const server = fastify({
 	logger: true
 });
 
+const customFileListRenderer: FileListRenderer = {
+	format: "html",
+	render: (dirs: Directory[], files: File[]): string => {
+		const dirsList = dirs.map(dir => {
+			return `<li><a href="${dir.href}">${dir.name}</a></li>`;
+		}).join("");
+		const filesList = files.map(file => {
+			return `<li><a href="${file.href}">${file.name}</a></li>`;
+		}).join("");
+		return `<html><body><h1>Directories</h1><ul>${dirsList}</ul><h1>Files</h1><ul>${filesList}</ul></body></html>`;
+	}
+};
+
 server.register(fastifyStatic, {
 	root: path.join(process.env.PWD, ROOT_DIR),
 	index: false,
-	list: true,
+	list: customFileListRenderer
 });
 
 server.listen(PORT, (err, address) => {
